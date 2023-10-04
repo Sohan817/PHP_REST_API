bce760b9db062c3dca2a8c584207bf38cd3ccfd4
Sohan817 <hsohan509@gmail.com>
2023-10-04 16:55:19 +0600

    Delete and search record in webpage using rest api

--- api_search.php
+++ api_search.php
@@ -3,20 +3,20 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 
 //Search using post method
-$data = json_decode(file_get_contents("php://input"), true);
-$search_value = $data['search'];
+// $data = json_decode(file_get_contents("php://input"), true);
+// $search_value = $data['search'];
 //Search using get method
-//$search_value = isset($_GET['search']) ? $_GET['search'] : die();
+$search_value = isset($_GET['search']) ? $_GET['search'] : die();
 
 require_once "database_connection/dbh.php";
 
-$query = "SELECT * FROM students where name LIKE '%{$search_value}%'";
+$query = "SELECT * FROM students WHERE name LIKE '%{$search_value}%'";
 
-$result = mysqli_query($connection, $query) or die(" SQL Query failed");
+$result = mysqli_query($connection, $query) or die("SQL Query failed");
 
 if (mysqli_num_rows($result) > 0) {
     $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
     echo json_encode($output);
 } else {
-    echo json_encode(array("message" => "No search result fount", "status" => false));
+    echo json_encode(array("message" => "No search result found", "status" => false));
 }
